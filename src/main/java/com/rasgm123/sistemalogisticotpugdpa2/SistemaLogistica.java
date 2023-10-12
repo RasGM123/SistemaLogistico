1799cfe1909fd2a6ff1546fd95931d47b917620c
Rodrigo <Rodrigo@DESKTOP-I0GV8J9.fibertel.com.ar>
2023-10-11 23:07:37 -0300

    Principal Base

--- src/main/java/com/rasgm123/sistemalogisticotpugdpa2/SistemaLogistica.java
+++ src/main/java/com/rasgm123/sistemalogisticotpugdpa2/SistemaLogistica.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.rasgm123.sistemalogisticotpugdpa2;
 
-import ventanas.*;
 
+import ventanas.general.*;
 
-/**
- *
- * @author Rodrigo
- */
 public class SistemaLogistica {
     public static void main(String[] args) {
-        // Crear una nueva instancia de la ventana principal
-        Login ventana = new Login();
-        ventana.setVisible(true);
+        General gen = new General();
+        gen.setVisible(true);
+        
     }
 }
--- src/main/java/ventanas/emergentes/aMovimiento.java
+++ src/main/java/ventanas/emergentes/aMovimiento.java
@@ -26,18 +26,22 @@ public class aMovimiento extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 428, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 196, Short.MAX_VALUE)
         );
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -78,5 +82,6 @@ public class aMovimiento extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }

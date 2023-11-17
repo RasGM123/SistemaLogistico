3a83ee13e27189e4aaf1a7b0f2f89c3e9cf67683
Gustavo <Gustavo@Notebook>
2023-11-17 14:49:54 -0300

    Clases Modelo

---
+++ src/main/java/com/mycompany/sistemalogisticotpugdpa2/SistemaLogisticoTPUGDPA2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemalogisticotpugdpa2;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class SistemaLogisticoTPUGDPA2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
